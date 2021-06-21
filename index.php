c7a5a54a4a537155e60c593335fee3bdd272c27e
10xMike <michelegec@gmail.com>
2021-06-21 15:42:05 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Bad Words</title>
+</head>
+<body>
+    <h1>Hello PHP</h1>
+
+    <?php 
+    //Creare una variabile con un paragrafo di testo a vostra scelta.
+    $p = "Usually when people ask how I’m doing, the real answer is I’m doing shitty, but I can’t say I’m doing shitty because I don’t even have a good reason to be doing shitty. So if I say, “I’m doing shitty,” then they say, “Why? What’s wrong?” And I have to be like, “I don’t know, all of it?” So instead, when people ask how I’m doing, I usually say, “I am doing so great.”";
+    #
+    $censorThis = $_GET["shitty"];
+    /*
+    
+    */
+
+
+
+    ?>
+
+</body>
+</html>
