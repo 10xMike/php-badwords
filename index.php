fc62eeb93047dcced8f676bd98c699da276b112f
10xMike <michelegec@gmail.com>
2021-06-21 15:57:07 +0200

    Add variables, echo

--- index.php
+++ index.php
@@ -8,17 +8,23 @@
 </head>
 <body>
     <h1>Hello PHP</h1>
+    
 
     <?php 
     //Creare una variabile con un paragrafo di testo a vostra scelta.
     $p = "Usually when people ask how I’m doing, the real answer is I’m doing shitty, but I can’t say I’m doing shitty because I don’t even have a good reason to be doing shitty. So if I say, “I’m doing shitty,” then they say, “Why? What’s wrong?” And I have to be like, “I don’t know, all of it?” So instead, when people ask how I’m doing, I usually say, “I am doing so great.”";
-    #
+    //Stampare a schermo il paragrafo e la sua lunghezza.
+    echo "<p>" . $p . "</p>";
+    echo 'Length of $p is - ' . strlen($p);
+
+    #Una parola da censurare viene passata dall'utente tramite parametro GET. 
     $censorThis = $_GET["shitty"];
-    /*
-    
-    */
 
+    #Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+    $censored_p = str_replace($censorThis, "***", $p);
 
+    echo "<p>" .$censored_p."</p>";
+    echo 'Length of $censored_p is - ' . strlen($censored_p);
 
     ?>
 
